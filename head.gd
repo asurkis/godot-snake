ebdd35a16e6408eb1e1f482f1ae1bc2d066649ee
Anton Surkis <antonsurkis@gmail.com>
2022-01-10 04:15:05 +0300

    Add tail collision

--- head.gd
+++ head.gd
@@ -1,6 +1,9 @@
-extends Sprite
+extends Area2D
+
+const Tail = preload("res://tail.tscn")
 
 var direction: int = 0
+var last_direction: int = 0
 const DELTAS = [
 	Vector2(1, 0),
 	Vector2(0, -1),
@@ -10,21 +13,28 @@ const DELTAS = [
 
 
 func _ready():
+	for _i in range(20):
+		$Tail.expand(Tail.instance())
 	$MoveTimer.start()
 
 
-func _process(delta):
-	if Input.is_action_pressed("ui_right"):
+func _process(_delta):
+	if   last_direction != 2 && Input.is_action_pressed("ui_right"):
 		direction = 0
-	elif Input.is_action_pressed("ui_up"):
+	elif last_direction != 3 && Input.is_action_pressed("ui_up"):
 		direction = 1
-	elif Input.is_action_pressed("ui_left"):
+	elif last_direction != 0 && Input.is_action_pressed("ui_left"):
 		direction = 2
-	elif Input.is_action_pressed("ui_down"):
+	elif last_direction != 1 && Input.is_action_pressed("ui_down"):
 		direction = 3
 	rotation_degrees = 90 * direction
 
 
 func _on_MoveTimer_timeout():
 	$Tail.move_tail(position)
-	position += 10 * DELTAS[direction]
+	position += 24 * DELTAS[direction]
+	last_direction = direction
+
+
+func _on_Head_area_entered(area):
+	print(area, area.is_in_group("tail"))
--- tail.gd
+++ tail.gd
@@ -12,4 +12,15 @@ func _ready():
 
 
 func move_tail(parent_pos: Vector2):
+	if next != null:
+		next.move_tail($Sprite.position)
 	$Sprite.position = parent_pos
+
+
+func expand(fragment):
+	if next == null:
+		next = fragment
+		next.move_tail($Sprite.position)
+		add_child(next)
+	else:
+		next.expand(fragment)
